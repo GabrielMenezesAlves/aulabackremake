965271c929b14becdb9d7b0b7a1f102c0159df9d
Gabriel M. Alves <gabriel_cm15@hotmail.com>
2024-12-03 21:08:13 -0300

    first commit

---
+++ exercicio1.js
@@ -0,0 +1,11 @@
+// inverter o texto
+// Oi Turma => amruT iO
+
+// contar vogais e consoantes
+// texto => vogais: 2 consoantes: 3
+
+// colocar o texto em ordem alfabetica
+// texto = eottx
+
+let texto = "Que dia ensolarado";
+
